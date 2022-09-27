666a16032db0642eb3e936f69519fd4b91b6f61d
deer <1770323038@qq.com>
2022-09-27 17:51:14 +0800

    初始化仓

---
+++ alivc-livepusher-demo/src/main/java/com/alivc/live/pusher/demo/LiveApplication.java
@@ -0,0 +1,63 @@
+package com.alivc.live.pusher.demo;
+
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+
+import com.alivc.live.pusher.LogUtil;
+
+import com.texeljoy.hteffect.HTEffect;
+
+
+public class LiveApplication extends Application {
+
+    public static Context sContext;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        sContext = this;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(new ConnectivityChangedReceiver(), filter);
+        if (BuildConfig.DEBUG) {
+            LogUtil.enalbeDebug();
+        } else {
+            LogUtil.disableDebug();
+        }
+
+        //todo --- hteffect start
+        HTEffect.shareInstance().initHTEffect( this, "0a5f04a23d3c40f2813da683a9eb55ec", new HTEffect.InitCallback() {
+            @Override public void onInitSuccess() {
+
+            }
+
+            @Override public void onInitFailure() {
+
+            }
+        });
+        //todo --- hteffect end
+
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            return;
+//        }
+//        LeakCanary.install(this);
+    }
+
+
+    class ConnectivityChangedReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    }
+
+}
